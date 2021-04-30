1b9abdc255cd7fc92944cf1a55aa429489d48557
Bărbuț-Dică Sami <70215066+WarriorsSami@users.noreply.github.com>
2021-04-30 11:15:43 +0300

    Update constants.php

--- Config/constants.php
+++ Config/constants.php
@@ -1,11 +1,11 @@
 <?php
 
-define ('EMAIL', 'usertechsavvy@gmail.com');
-define ('PASSWORD', 'firmaitinfo');
+define ('EMAIL', '');
+define ('PASSWORD', '');
 define ('DATABASE_SQL', 'userboard');
 //define ('USERNAME_SQL', 'root');
 //define ('PASSWORD_SQL', '');
 //define ('HOSTNAME_SQL', 'localhost');
-define ('USERNAME_SQL', 'SamiAdmin');
-define ('PASSWORD_SQL', 'Sami2002$$');
+define ('USERNAME_SQL', '');
+define ('PASSWORD_SQL', '');
 define ('HOSTNAME_SQL', 'SG-ClusterPHP-4183-mysql-master.servers.mongodirector.com');
